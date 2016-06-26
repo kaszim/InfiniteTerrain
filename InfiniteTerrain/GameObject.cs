94d845ded349a10a283148ef71e3e6ac8ce91bad
kasimt <kasimt@kth.se>
2016-06-26 18:01:00 +0200

    Added comments to GameObject

--- InfiniteTerrain/GameObject.cs
+++ InfiniteTerrain/GameObject.cs
@@ -5,25 +5,45 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace InfiniteTerrain
 {
+    /// <summary>
+    /// A game object.
+    /// This class holds information about objects that are in the world.
+    /// They collide with the terrain.
+    /// </summary>
     class GameObject
     {
+        // Acceleration of this game object
         private float acceleration = 2000f;
+        // The gravity factor (how much pull there is on the object)
         private float gravityFactor = 1000f;
+        // The dampening factor, e.g how much friction the air is making
         private float dampeningFactor = 8f;
+        // Position of the object
         private Vector2 position;
+        // The object's velocity
         private Vector2 velocity;
+        // Size of the object
         private Point size;
 
+        /// <summary>
+        /// The GameObject's position in the world.
+        /// </summary>
         public Vector2 Position
         {
             get { return position; }
             set { position = value; }
         }
+        /// <summary>
+        /// The GameObject's velocity. In pixels/second
+        /// </summary>
         public Vector2 Velocity
         {
             get { return velocity; }
             set { velocity = value; }
         }
+        /// <summary>
+        /// The GameObject's bounding rectangle.
+        /// </summary>
         public Rectangle Rectangle
         {
             get { return new Rectangle((int)position.X, (int)position.Y, size.X, size.Y); }
@@ -35,6 +55,9 @@ namespace InfiniteTerrain
             this.size = size;
         }
 
+        /// <summary>
+        /// Updates the GameObject. Applies physics and such.
+        /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime)
         {
@@ -52,6 +75,9 @@ namespace InfiniteTerrain
                 velocity.Y -= acceleration * deltaTime;
         }
 
+        /// <summary>
+        /// Updates physics.
+        /// </summary>
         protected void updatePhysics(GameTime gameTime)
         {
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -62,9 +88,14 @@ namespace InfiniteTerrain
             position += velocity * deltaTime;
         }
 
+        /// <summary>
+        /// Draws the GameObject to the screen.
+        /// </summary>
+        /// <param name="spriteBatch">The spriteBatch to draw to.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
-            C3.XNA.Primitives2D.FillRectangle(spriteBatch, Camera.WorldToScreenRectangle(Rectangle), Color.BlueViolet);
+            C3.XNA.Primitives2D.FillRectangle(spriteBatch, Camera.WorldToScreenRectangle(Rectangle),
+                Color.BlueViolet);
         }
     }
 }
