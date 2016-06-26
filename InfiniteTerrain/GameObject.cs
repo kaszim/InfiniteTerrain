fae174d4f40ca213d9d4c51dc4aa206637601609
kasimt <kasimt@kth.se>
2016-06-26 17:48:27 +0200

    Added velocity to gameobject

--- InfiniteTerrain/GameObject.cs
+++ InfiniteTerrain/GameObject.cs
@@ -7,13 +7,22 @@ namespace InfiniteTerrain
 {
     class GameObject
     {
+        private float acceleration = 2000f;
+        private float gravityFactor = 1000f;
+        private float dampeningFactor = 8f;
         private Vector2 position;
+        private Vector2 velocity;
         private Point size;
 
         public Vector2 Position
         {
             get { return position; }
             set { position = value; }
+        }
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
         }
         public Rectangle Rectangle
         {
@@ -29,17 +38,28 @@ namespace InfiniteTerrain
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime)
         {
+            updatePhysics(gameTime);
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             var keyState = Keyboard.GetState();
             if (keyState.IsKeyDown(Keys.Right))
-                position = new Vector2(position.X + 300 * deltaTime, position.Y);
+                velocity.X += acceleration * deltaTime;
             if (keyState.IsKeyDown(Keys.Left))
-                position = new Vector2(position.X - 300 * deltaTime, position.Y);
+                velocity.X -= acceleration * deltaTime;
             if (keyState.IsKeyDown(Keys.Down))
-                position = new Vector2(position.X, position.Y + 300 * deltaTime);
+                velocity.Y += acceleration * deltaTime;
             if (keyState.IsKeyDown(Keys.Up))
-                position = new Vector2(position.X, position.Y - 300 * deltaTime);
+                velocity.Y -= acceleration * deltaTime;
+        }
+
+        protected void updatePhysics(GameTime gameTime)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            velocity -= velocity * dampeningFactor * deltaTime;
+            // Gravity disabled for now.
+            //velocity.X -= velocity.X * dampeningFactor * deltaTime;
+            //velocity.Y += gravityFactor * deltaTime;
+            position += velocity * deltaTime;
         }
 
         public void Draw(SpriteBatch spriteBatch)
