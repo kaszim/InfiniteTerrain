3cf867c52e17527ed95cfda8e7a89f7d6a3c924f
Kasim Taskin <kasimt@kth.se>
2016-06-28 04:02:39 +0200

    GameObject's are now encapsulated inside of World, added Player class

--- InfiniteTerrain/GlobalSuppressions.cs
+++ InfiniteTerrain/GlobalSuppressions.cs
@@ -4,8 +4,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0017:Use auto property", Justification = "<Pending>", Scope = "member", Target = "~P:InfiniteTerrain.GameObject.Position")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0017:Use auto property", Justification = "<Pending>", Scope = "member", Target = "~P:InfiniteTerrain.GameObject.Velocity")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0017:Use auto property", Justification = "<Pending>", Scope = "member", Target = "~P:InfiniteTerrain.Camera.Position")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0014:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:InfiniteTerrain.World.Update(Microsoft.Xna.Framework.GameTime)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0052:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:InfiniteTerrain.GameObject.acceleration")]
--- InfiniteTerrain/World.cs
+++ InfiniteTerrain/World.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
+using InfiniteTerrain.GameObjects;
 
 namespace InfiniteTerrain
 {
@@ -10,7 +13,7 @@ namespace InfiniteTerrain
         private readonly GraphicsDevice graphicsDevice;
         private SpriteBatch spriteBatch;
         private readonly Terrain terrain;
-        private readonly GameObject o;
+        private readonly HashSet<IGameObject> gameObjects;
 
         /// <summary>
         /// Creates and initializes a World.
@@ -20,8 +23,7 @@ namespace InfiniteTerrain
         {
             this.graphicsDevice = graphicsDevice;
             terrain = new Terrain(graphicsDevice, 10000, 5000);
-            o = new GameObject(new Vector2(500, 500), new Point(25, 75));
-
+            gameObjects = new HashSet<IGameObject>();
         }
 
         /// <summary>
@@ -31,6 +33,8 @@ namespace InfiniteTerrain
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(graphicsDevice);
+            for(int i = 0; i < 50; i++)
+                gameObjects.Add(new Player());
         }
 
         /// <summary>
@@ -40,27 +44,30 @@ namespace InfiniteTerrain
         public void Update(GameTime gameTime)
         {
             terrain.Update(gameTime);
-            o.Update(gameTime);
-            var recs = terrain.GetCollidingRectangles(o.Rectangle, QuadTreeType.Texture);
-            foreach (Rectangle other in recs)
+            foreach (var gObject in gameObjects)
             {
-                var dCenter = o.Rectangle.Center - other.Center;
-                float dx;
-                float dy;
-                // Check which side we intersect from and calculate the dx accordingly
-                if (dCenter.X > 0)
-                    dx = other.X + other.Size.X - o.Position.X;
-                else
-                    dx = other.X - o.Position.X - o.Rectangle.Width;
-                // same thing here but with dy
-                if (dCenter.Y > 0)
-                    dy = other.Y + other.Size.Y - o.Position.Y;
-                else
-                    dy = other.Y - o.Position.Y - o.Rectangle.Height;
-                // Whichever of the d's are smallest should be solved first, leave the other for
-                // next update
-                var dPos = Math.Abs(dx) > Math.Abs(dy) ? new Vector2(0, dy) : new Vector2(dx, 0);
-                o.Position += dPos;
+                gObject.Update(gameTime);
+                var recs = terrain.GetCollidingRectangles(gObject.Rectangle, QuadTreeType.Texture);
+                foreach (Rectangle other in recs)
+                {
+                    var dCenter = gObject.Rectangle.Center - other.Center;
+                    float dx;
+                    float dy;
+                    // Check which side we intersect from and calculate the dx accordingly
+                    if (dCenter.X > 0)
+                        dx = other.X + other.Size.X - gObject.Position.X;
+                    else
+                        dx = other.X - gObject.Position.X - gObject.Rectangle.Width;
+                    // same thing here but with dy
+                    if (dCenter.Y > 0)
+                        dy = other.Y + other.Size.Y - gObject.Position.Y;
+                    else
+                        dy = other.Y - gObject.Position.Y - gObject.Rectangle.Height;
+                    // Whichever of the d's are smallest should be solved first, leave the other for
+                    // next update
+                    var dPos = Math.Abs(dx) > Math.Abs(dy) ? new Vector2(0, dy) : new Vector2(dx, 0);
+                    gObject.Position += dPos;
+                }
             }
         }
 
@@ -72,8 +79,16 @@ namespace InfiniteTerrain
         {
             terrain.Draw(gameTime);
             spriteBatch.Begin();
-            o.Draw(spriteBatch);
+            foreach (var gObject in gameObjects)
+            {
+                gObject.Draw(spriteBatch);
+            }
             spriteBatch.End();
+        }
+
+        public void Add(GameObject o)
+        {
+            gameObjects.Add(o);
         }
     }
 }
