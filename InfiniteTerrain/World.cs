092f4c7f883ed043ec464a9762eaf1a4768aafab
Kasim Taskin <kasimt@kth.se>
2016-06-28 04:07:03 +0200

    Collision check is now done within gameobject class

--- InfiniteTerrain/World.cs
+++ InfiniteTerrain/World.cs
@@ -33,8 +33,8 @@ namespace InfiniteTerrain
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(graphicsDevice);
-            for(int i = 0; i < 50; i++)
-                gameObjects.Add(new Player());
+            for(int i = 0; i < 500; i++)
+                Add(new Player());
         }
 
         /// <summary>
@@ -47,27 +47,6 @@ namespace InfiniteTerrain
             foreach (var gObject in gameObjects)
             {
                 gObject.Update(gameTime);
-                var recs = terrain.GetCollidingRectangles(gObject.Rectangle, QuadTreeType.Texture);
-                foreach (Rectangle other in recs)
-                {
-                    var dCenter = gObject.Rectangle.Center - other.Center;
-                    float dx;
-                    float dy;
-                    // Check which side we intersect from and calculate the dx accordingly
-                    if (dCenter.X > 0)
-                        dx = other.X + other.Size.X - gObject.Position.X;
-                    else
-                        dx = other.X - gObject.Position.X - gObject.Rectangle.Width;
-                    // same thing here but with dy
-                    if (dCenter.Y > 0)
-                        dy = other.Y + other.Size.Y - gObject.Position.Y;
-                    else
-                        dy = other.Y - gObject.Position.Y - gObject.Rectangle.Height;
-                    // Whichever of the d's are smallest should be solved first, leave the other for
-                    // next update
-                    var dPos = Math.Abs(dx) > Math.Abs(dy) ? new Vector2(0, dy) : new Vector2(dx, 0);
-                    gObject.Position += dPos;
-                }
             }
         }
 
@@ -88,7 +67,9 @@ namespace InfiniteTerrain
 
         public void Add(GameObject o)
         {
-            gameObjects.Add(o);
+            var interfaceO = (IGameObject)o;
+            interfaceO.Initialize(terrain, this);
+            gameObjects.Add(interfaceO);
         }
     }
 }
